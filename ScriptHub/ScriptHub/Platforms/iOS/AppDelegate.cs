35c1a3a99d4be8e6a10d43fa9a745853f7384956
JoaoPedroSSilva <97jpss@gmail.com>
2025-11-26 14:56:35 -0300

    Arquivos Visual Studio Maui

---
+++ ScriptHub/ScriptHub/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScriptHub
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ ScriptHub/ScriptHub/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ScriptHub
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

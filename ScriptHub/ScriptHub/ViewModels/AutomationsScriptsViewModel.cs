08c4879cac5f3dd92b7a06c9e64270e4a18de802
JoaoPedroSSilva <97jpss@gmail.com>
2025-11-28 17:56:00 -0300

    Criado e relacionado os Views Models

---
+++ ScriptHub/ScriptHub/ViewModels/AutomationsScriptsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ScriptHub.ViewModels
+{
+    public partial class AutomationsScriptsViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private async Task GoToMainPage()
+        {
+            await Shell.Current.GoToAsync("///MainPage");
+        }
+    }
+}
